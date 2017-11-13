bee63f3fdf4895a0a96c5adc651b004c87644c55
Eric Marty <contact@ericmarty.co>
2017-11-12 17:45:50 -0800

    Enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject {
+
+    public int playerDamage;
+
+    private Animator animator;
+    // Store the players positions and use it to move there
+    private Transform target;
+    // Cause the enemy to move every other turn
+    private bool skipMove;
+
+	protected override void Start () {
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start();
+	}
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        if (skipMove)
+        {
+            skipMove = false;
+            return;
+        }
+
+        base.AttemptMove<T>(xDir, yDir);
+
+        skipMove = true;
+    }
+
+    // To be called in gameManager when gameManager issues  request for an enemy to move
+    public void MoveEnemy()
+    {
+        int xDir = 0;
+        int yDir = 0;
+
+        // Are our enemy and player in the same column
+        if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+            yDir = target.position.y > transform.position.y ? 1 : -1;
+        else
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+
+        AttemptMove<Player>(xDir, yDir);
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Player hitPlayer = component as Player;
+
+        hitPlayer.LoseFood(playerDamage);
+    }
+}
